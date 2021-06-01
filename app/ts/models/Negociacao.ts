eeff4ff3c368c29f5260eff431cc0a174d88f1af
dirceu junior <dirceu.sjr@gmail.com>
2021-06-01 12:28:42 -0300

    parâmetros readonly

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,13 +1,7 @@
 export class Negociacao {
 
-    constructor(private _data: Date, private _quantidade: number, private _valor: number) { }
+    constructor(readonly data: Date, readonly quantidade: number, readonly valor: number) { }
 
-    get data() { return this._data; }
-    get quantidade() { return this._quantidade; }
-    get valor() { return this._valor; }
-
-    get volume() { return this._quantidade * this._valor; }
-
-
+    get volume() { return this.quantidade * this.valor; }
 
 }
